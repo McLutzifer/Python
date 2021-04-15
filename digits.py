7c224eaf1c0427722087c7b0bd7c57f0ca1ffa0f
McLutzifer <lukasschumi@gmx.at>
2021-04-15 15:59:28 +0200

    update

--- digits.py
+++ digits.py
@@ -3,7 +3,7 @@ line_2 = "  |  _|  _| |_| |_  |_    | |_| |_| | | "
 line_3 = "  | |_   _|   |  _| |_|   | |_|  _| |_| "
 
 i = 0
-while i < 31:       # 27 characters + 9 spaces = 36 digits 
+while i < 33:       # 27 characters + 9 spaces = 36 digits 
     print(line_1[i:i+3])
     print(line_2[i:i+3])
     print(line_3[i:i+3])
