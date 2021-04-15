dba8f27892f39a0992c586f73ba74ae1eb91b487
McLutzifer <lukasschumi@gmx.at>
2021-04-15 15:56:27 +0200

    update

--- digits.py
+++ digits.py
@@ -1,8 +1,10 @@
-line_1 = "     _  _       _   _   _   _   _   _  "
-line_2 = "  |  _| _| |_| |_  |_    | |_| |_| | | "
-line_3 = "  | |_  _|   |  _| |_|   | |_|  _| |_| "
+line_1 = "     _   _       _   _   _   _   _   _  "
+line_2 = "  |  _|  _| |_| |_  |_    | |_| |_| | | "
+line_3 = "  | |_   _|   |  _| |_|   | |_|  _| |_| "
 
 i = 0
-while i < 25:
+while i < 31:       # 27 characters + 9 spaces = 36 digits 
+    print(line_1[i:i+3])
     print(line_2[i:i+3])
-    i += 1
+    print(line_3[i:i+3])
+    i += 4
