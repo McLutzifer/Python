25a0d92fbe6dbe312b1a57cfe7b91dbdec92dbda
McLutzifer <lukasschumi@gmx.at>
2021-04-15 16:32:36 +0200

    update

--- digits.py
+++ digits.py
@@ -2,69 +2,94 @@ line_1 = "     _   _       _   _   _   _   _   _  "
 line_2 = "  |  _|  _| |_| |_  |_    | |_| |_| | | "
 line_3 = "  | |_   _|   |  _| |_|   | |_|  _| |_| "
 
+one = [
+    "   ",
+    "  |",
+    "  |"
+]
+
+two = [
+    " _ ",
+    " _|",
+    "|_ "
+]
+
+three = [
+    " _ ",
+    " _|",
+    " _|"
+]
+
+four = [
+    "   ",
+    "|_|",
+    "  |"
+]
+
+five = [
+    " _ ",
+    "|_ ",
+    " _|"
+]
+
+six = [
+    " _ ",
+    "|_ ",
+    "|_|"
+]
+
+seven = [
+    " _ ",
+    "  |",
+    "  |"
+]
+
+eight = [
+    " _ ",
+    "|_|",
+    "|_|"
+]
+
+nine = [
+    " _ ",
+    "|_|",
+    " _|"
+]
+
+zero = [
+    " _ ",
+    "| |",
+    "|_|"
+]
+
+digits = [one, two, three, four, five, six, seven, eight, nine, zero]
+
+row_of_numbers = []
 i = 0
 while i < 33:       # 27 characters + 9 spaces = 36 digits 
     print(line_1[i:i+3])
     print(line_2[i:i+3])
     print(line_3[i:i+3])
+
+
+    temp = []
+
+    temp.append(line_1[i:i+3])
+    temp.append(line_2[i:i+3])
+    temp.append(line_3[i:i+3])
+
+    print(temp)
+
+    if temp == one:
+        print("XXXXXXXXXXXXXXXXXXXXXXXXX")
+
+    for number in digits:
+        if temp == number:
+            print(digits.index(number) +1)
+            row_of_numbers.append(digits.index(number) +1)
+
+
+    
     i += 4
 
-one = {
-    {"   "},
-    {"  |"},
-    {"  |"}
-}
-
-two = {
-    {" _ "},
-    {" _|"},
-    {"|_ "}
-}
-
-three = {
-    {" _ "},
-    {" _|"},
-    {" _|"}
-}
-
-four = {
-    {"   "},
-    {"|_|"},
-    {"  |"}
-}
-
-five = {
-    {" _ "},
-    {"|_"},
-    {" _|"}
-}
-
-six = {
-    {" _ "},
-    {"|_ "},
-    {"|_|"}
-}
-
-seven = {
-    {" _ "},
-    {"  |"},
-    {"  |"}
-}
-
-eight = {
-    {" _ "},
-    {"|_|"},
-    {"|_|"}
-}
-
-nine = {
-    {" _ "},
-    {"|_|"},
-    {" _|"}
-}
-
-zero = {
-    {" _ "},
-    {"| |"},
-    {"|_|"}
-}
+print(row_of_numbers)
