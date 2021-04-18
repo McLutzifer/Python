e6712c8efe1e77bb30fd8c26341c04dc2f5e86ee
McLutzifer <lukasschumi@gmx.at>
2021-04-18 19:25:57 +0200

    addlines

--- digits.py
+++ digits.py
@@ -70,7 +70,7 @@ zero = [
 
 digits = [one, two, three, four, five, six, seven, eight, nine, zero]
 
-line = 0 # starting at first line and inspecting 3 at a time
+
 
 '''
 
@@ -95,7 +95,7 @@ while True:
 
 file.close()
 '''
-
+'''
 row_of_numbers = []
 i = 0
 while i < 33:       # 27 characters + 9 spaces = 36 digits 
@@ -116,10 +116,35 @@ while i < 33:       # 27 characters + 9 spaces = 36 digits
 
 print(row_of_numbers)
 print("Test")
+'''
 
 
 def read_file(filename):
 
+    row_of_numbers = []
+    line= 0 # starting at first line and inspecting 3 at a time
+    i = 0
+
+    ##########
+    with open(filename, 'r') as file:
+    
+    # check iflines in file %4 == 0
+
+    while i < 33:       # 27 characters + 9 spaces = 36 digits 
+        temp = []
+
+        temp.append(line_1[i:i+3])
+        temp.append(line_2[i:i+3])
+        temp.append(line_3[i:i+3])
+
+        for number in digits:
+            if temp == number:
+                row_of_numbers.append(digits.index(number) +1)
+            # user story 3
+            else:
+                row_of_numbers.append("?")   #####??????????
+
+        i += 4
 
     return row_of_numbers
 
