4654cd374107e690a8a15e5bf0e587415c4ed023
McLutzifer <lukasschumi@gmx.at>
2021-04-19 19:11:11 +0200

    what?

--- BANK/writer.py
+++ BANK/writer.py
@@ -1,8 +1,11 @@
 def write(account):
 
+#    print(account)
+    
     f = open("account_numbers.txt", "a")
 
     for digit in account:
         f.write(digit)
     f.write("\n")
     f.close()
+    
