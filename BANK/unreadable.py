5a7fbbc45fc6b1d19b06b4e5763ca290d7d4e3e3
McLutzifer <lukasschumi@gmx.at>
2021-04-20 12:30:58 +0200

    unreadable

--- BANK/unreadable.py
+++ BANK/unreadable.py
@@ -1,6 +1,24 @@
+import digits
+
+
+test = [
+    "   ",
+    "| |",
+    "  |"
+]
+
 
 def guessing_the_number(integer):
 
+    testcase = integer[0] + integer[1] + integer[2]
+    print(testcase)
+    # "  | |  |"
+
     
 
+
+
+
     return integer
+
+guessing_the_number(test)
