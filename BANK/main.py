0932005ef0fc3ac9ac782fecab736e929ecfe21a
McLutzifer <lukasschumi@gmx.at>
2021-04-20 19:20:43 +0200

    searching bug from Task 4

--- BANK/main.py
+++ BANK/main.py
@@ -4,7 +4,7 @@ import reader, writer, wrong_checksum, checksum
 #    USER STORY 1    #
 ######################
 
-my_test_file = "/home/lukas/Documents/Programming/Python/BANK/testfile.txt"
+my_test_file = "testfile.txt"
 read_digits = reader.read_file(my_test_file)
 account_numbers = reader.parse_dictionary(read_digits)
 
