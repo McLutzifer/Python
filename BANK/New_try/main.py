ba3e7a3a798668273565fb744cf878284a30704d
McLutzifer <lukasschumi@gmx.at>
2021-04-22 14:36:41 +0200

    split into single functions again

--- BANK/New_try/main.py
+++ BANK/New_try/main.py
@@ -1,3 +1,19 @@
-import digits
+import digits, reader
+
+
+######################
+#    USER STORY 1    #
+######################
+
 
 my_test_file = "/home/lukas/Documents/Programming/Python/BANK/New_try/testfile.txt"
+
+read_digits = reader.read_file(my_test_file)
+
+
+
+
+
+
+account_numbers = reader.parse_dictionary(read_digits)
+
