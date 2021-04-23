8c0852b52e246bd3764bab075c87c4790812b788
McLutzifer <lukasschumi@gmx.at>
2021-04-23 17:11:51 +0200

    elif instead of is's

--- BANK/New_try/translator.py
+++ BANK/New_try/translator.py
@@ -5,12 +5,12 @@ def translate(hex):    #translates input in row of digits
         for j in range(3):
             if hex[i][j] == " ":
                 number.append(0)
-            if hex[i][j] == "|":
+            elif hex[i][j] == "|":
                 number.append(1)
-            if hex[i][j] == "_":
+            elif hex[i][j] == "_":
                 number.append(2)
-            if hex[i][j] == "\\":
+            elif hex[i][j] == "\\":
                 number.append(3)
-            if hex[i][j] == "/":
+            elif hex[i][j] == "/":
                 number.append(4)
     return number
