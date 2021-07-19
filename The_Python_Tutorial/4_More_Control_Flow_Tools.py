26e3f2c67dc7e22d80c6ddd17330b8986cd79353
McLutzifer <lukasschumi@gmx.at>
2021-07-19 17:17:01 +0200

    def function

--- The_Python_Tutorial/4_More_Control_Flow_Tools.py
+++ The_Python_Tutorial/4_More_Control_Flow_Tools.py
@@ -33,3 +33,14 @@ for num in range(2, 10):
         print("Found an even number", num)
         continue
     print("Found an odd number" , num)
+
+
+def fib(n):    # write Fibonacci series up to n
+    """Print a Fibonacci series up to n."""
+    a, b = 0, 1
+    while a < n:
+        print(a, end=' ')
+        a, b = b, a+b
+    print()
+
+fib(200)
