cb64ffe90a9c5f31503a3678c60383805f98173f
McLutzifer <lukasschumi@gmx.at>
2021-07-19 17:33:54 +0200

    in

--- The_Python_Tutorial/4_More_Control_Flow_Tools.py
+++ The_Python_Tutorial/4_More_Control_Flow_Tools.py
@@ -51,9 +51,24 @@ def fib2(n):    # return Fibonacci series up to n
     result = []
     a, b = 0, 1
     while a < n:
-        result.append(a)    # see below
+        result.append(a)
         a, b = b, a+b
     return result
 
 fib100 = fib2(100)
 print(fib100)
+
+
+def ask_ok(prompt, retries=4, reminder='Please try again!'):
+    while True:
+        ok = input(prompt)
+        if ok in ('y', 'ye', 'yes'):
+            return True
+        if ok in ('n', 'no', 'nop', 'nope'):
+            return False
+        retries -= 1
+        if retries < 0:
+            raise ValueError('invalid user response')
+        print(reminder)
+
+ask_ok('Do you really want to quit?', 2,"C'mon")
