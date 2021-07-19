a462ccef58ae815dbc6b1cd9e9c8b1a906d7b378
McLutzifer <lukasschumi@gmx.at>
2021-07-19 17:01:50 +0200

    strings

--- The_Python_Tutorial/3_An-Informal_Introduction.py
+++ The_Python_Tutorial/3_An-Informal_Introduction.py
@@ -2,3 +2,12 @@ print("# This is not a comment because it's inside quotes.")
 
 print(17/3)
 
+print("""\
+Usage: thingy [OPTIONS]
+     -h                        Display this usage message
+     -H hostname               Hostname to connect to
+""")
+
+word = 'Ananananananas'
+
+print(word[0::2])
