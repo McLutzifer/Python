e20c6dd55ef6f288cd17d5e95c765dd7a2a9c012
McLutzifer <lukasschumi@gmx.at>
2021-07-19 17:14:45 +0200

    continue

--- The_Python_Tutorial/4_More_Control_Flow_Tools.py
+++ The_Python_Tutorial/4_More_Control_Flow_Tools.py
@@ -24,6 +24,12 @@ for n in range(2, 10):
         if n % x == 0:
             print(n, 'equals', x, '*', n//x)
             break
-        else:
-            # loop fell trough without finding a factor
-            print(n, 'is a prime number')
+    else:
+        # loop fell trough without finding a factor
+        print(n, 'is a prime number')
+
+for num in range(2, 10):
+    if num % 2 == 0:
+        print("Found an even number", num)
+        continue
+    print("Found an odd number" , num)
