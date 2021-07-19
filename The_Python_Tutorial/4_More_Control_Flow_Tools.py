4daff3401d4552e2f0630ec944b1d68a4a59e310
McLutzifer <lukasschumi@gmx.at>
2021-07-19 17:08:12 +0200

    range

--- The_Python_Tutorial/4_More_Control_Flow_Tools.py
+++ The_Python_Tutorial/4_More_Control_Flow_Tools.py
@@ -1,1 +1,19 @@
-integer = 42
+
+'''
+# Strategy:  Iterate over a copy
+for user, status in users.copy().items():
+    if status == 'inactive':
+        del users[user]
+
+# Strategy:  Create a new collection
+active_users = {}
+for user, status in users.items():
+    if status == 'active':
+        active_users[user] = status
+'''
+
+for i in range(5):
+    print(i)
+
+newlist = list(range(5, 10))
+print(newlist)
