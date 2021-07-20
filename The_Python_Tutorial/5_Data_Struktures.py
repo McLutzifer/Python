aa9c47e65764c5dd64a9fa6b34a67daaee6e81f3
McLutzifer <lukasschumi@gmx.at>
2021-07-20 21:17:28 +0200

    More on conditions

--- The_Python_Tutorial/5_Data_Struktures.py
+++ The_Python_Tutorial/5_Data_Struktures.py
@@ -67,3 +67,13 @@ for value in raw_data:
         filtered_data.append(value)
 
 print(filtered_data)
+
+
+
+#########################
+#      Conditions       #
+#########################
+
+string1, string2, string3 = '', 'Trondheim', 'Hammer Dance'
+non_null = string1 or string2 or string3
+print(non_null)
