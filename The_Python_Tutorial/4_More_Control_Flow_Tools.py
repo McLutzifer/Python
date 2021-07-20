00a4360a1ea57a8555baa33174d5abe11721a785
McLutzifer <lukasschumi@gmx.at>
2021-07-20 18:28:17 +0200

    Arbitrary Argument Lists

--- The_Python_Tutorial/4_More_Control_Flow_Tools.py
+++ The_Python_Tutorial/4_More_Control_Flow_Tools.py
@@ -80,4 +80,18 @@ ask_ok('Do you really want to quit?', 2,"C'mon")
 
 def cheeseshop(kind, *arguments, **keywords):
     print("-- Do you have any", kind, "?")
-    print("-- I'm sorry")
+    print("-- I'm sorry, we're all out of", kind)
+    for arg in arguments:
+        print(arg)
+    print("-" * 40)
+    for kw in keywords:
+        print(kw, ":", keywords[kw])
+
+cheeseshop("Limburger", "It's very runny sir." , 
+            "It's really very, VERY runny sir.",
+            shopkeeper="Michael Palin",
+            client="John Cleese",
+            sketch="Cheese Shop Sketch")
+
+
+def 
