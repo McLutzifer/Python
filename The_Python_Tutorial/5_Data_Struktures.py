ed3cef27a913e3cf4dfc03e6f21276f6a331c669
McLutzifer <lukasschumi@gmx.at>
2021-07-20 21:14:39 +0200

    looping techniques

--- The_Python_Tutorial/5_Data_Struktures.py
+++ The_Python_Tutorial/5_Data_Struktures.py
@@ -58,3 +58,12 @@ questions = ['name', 'quest', 'favorite color']
 answers = ['lancelot', 'the holy grail', 'blue']
 for q, a in zip(questions, answers):
     print('What is your {0}? It is {1}.'.format(q, a))
+
+import math
+raw_data = [56.2, float('NaN'), 51.7, 55.3, 53.5, float('NaN'), 47.8]
+filtered_data = []
+for value in raw_data:
+    if not math.isnan(value):
+        filtered_data.append(value)
+
+print(filtered_data)
