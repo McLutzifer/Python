bad1c4ce5b4a965666956f6a5496d2cbfdfa7e7f
McLutzifer <lukasschumi@gmx.at>
2021-07-20 21:10:01 +0200

    looping techniques

--- The_Python_Tutorial/5_Data_Struktures.py
+++ The_Python_Tutorial/5_Data_Struktures.py
@@ -50,3 +50,11 @@ knights = {'gallahad': 'the pure', 'robin': 'the brave'}
 for k, v in knights.items():
     print(k, v)
     print('---------')
+
+for i, v in enumerate(['tic', 'tac', 'toe']):
+    print(i, v)
+
+questions = ['name', 'quest', 'favorite color']
+answers = ['lancelot', 'the holy grail', 'blue']
+for q, a in zip(questions, answers):
+    print('What is your {0}? It is {1}.'.format(q, a))
