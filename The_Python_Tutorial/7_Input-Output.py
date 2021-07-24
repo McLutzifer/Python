da9ad087cc0e866ee0e9379b045401181d8a7361
McLutzifer <lukasschumi@gmx.at>
2021-07-24 13:18:11 +0200

    Manual String Formatting

--- The_Python_Tutorial/7_Input-Output.py
+++ The_Python_Tutorial/7_Input-Output.py
@@ -33,3 +33,9 @@ print('The story of {1}, {0}, and {other}.'.format('Bill', 'Manfred', other='Geo
 
 for x in range(1, 11):
      print('{0:2d} {1:3d} {2:4d}'.format(x, x*x, x*x*x))
+
+
+for x in range(1, 11):
+    print(repr(x).rjust(2), repr(x*x).rjust(3), end=' ')
+    # Note use of 'end' on previous line
+    print(repr(x*x*x).rjust(4))
