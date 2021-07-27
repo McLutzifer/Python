5ff1736310cbe8e27bfe68bba2e629ffd46ebcf9
McLutzifer <lukasschumi@gmx.at>
2021-07-27 18:32:53 +0200

    add 'add_edge' method

--- Solo-Learn_Data-structures/Graph.py
+++ Solo-Learn_Data-structures/Graph.py
@@ -3,3 +3,10 @@ class Graph():
         self.adj = [ [0] * size for i in range(size)]
         self.size = size
 
+    def add_edge(self, orig, dest):
+        if orig > self.size or dest > self.size or orig < 0 or dest < 0:
+            print("Invalid Edge")
+        else:
+            self.adj[orig-1][dest-1] = 1
+            self.adj[dest-1][orig-1] = 1
+
