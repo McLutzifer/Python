a8ccfdefb5f19ae6d1dedc24e3fd8080e18a8aca
McLutzifer <lukasschumi@gmx.at>
2021-07-27 18:34:32 +0200

    add 'remove_edge' method

--- Solo-Learn_Data-structures/Graph.py
+++ Solo-Learn_Data-structures/Graph.py
@@ -10,3 +10,10 @@ class Graph():
             self.adj[orig-1][dest-1] = 1
             self.adj[dest-1][orig-1] = 1
 
+    def remove_edge(self, orig, dest):
+        if orig > self.size or dest > self.size or orig < 0 or dest < 0:
+            print("Invalid Edge")
+        else:
+            self.adj[orig-1][dest-1] = 0
+            self.adj[dest-1][orig-1] = 0
+
