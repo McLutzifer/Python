2bbdddb4092f817eddc4d78672747b5fffb6b034
McLutzifer <lukasschumi@gmx.at>
2021-08-03 18:17:30 +0200

    import base64

--- THM/Challange.py
+++ THM/Challange.py
@@ -8,3 +8,9 @@ print("TEST")
 5 times encoded using base 32
 5 times encoded using base 16!
 '''
+
+file = open('encodedflag.txt', 'r')
+text = file.read()
+
+
+encodedStr = base64encode(file.encode("utf-8"))
