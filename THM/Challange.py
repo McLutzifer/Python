e23f191471b3534a412c7db10845aadebf847d74
McLutzifer <lukasschumi@gmx.at>
2021-08-03 18:55:45 +0200

    try brute force

--- THM/Challange.py
+++ THM/Challange.py
@@ -2,42 +2,47 @@
 import base64
 import pybase64
 
-print("TEST")
-
-global x
 
 '''
 5 times encoded using base 64
 5 times encoded using base 32
 5 times encoded using base 16!
-'''
-def code64(x):
-    for i in range (5):
-        x = base64.b64encode(x)
-    return x
+
 
 def code32(x):
-    encodedStr = base64.b64encode(s)
-    return encodedStr
+    x = base64.b64encode(x)
+    return x
 
 def code16(x):
-    encodedStr = base64.b64encode(s)
-    return encodedStr
+    x = base64.b64encode(x)
+    return x
+'''
 
 
 file = open('encodedflag.txt', 'r')
 text = file.read()
 
-s = text.encode("UTF-8")
+x = text.encode("UTF-8")
 
-for i in range (5):
-    x = code64(x)
+x = base64.b64encode(x)
+'''
+x = base64.b64encode(x)
+x = base64.b64encode(x)
+x = base64.b64encode(x)
+x = base64.b64encode(x)
 
-for i in range (5):
-    x = code64(x)
+x = base64.b32encode(x)
+x = base64.b32encode(x)
+x = base64.b32encode(x)
+x = base64.b32encode(x)
+x = base64.b32encode(x)
 
-for i in range (5):
-    x = code64(x)
+x = base64.b16encode(x)
+x = base64.b16encode(x)
+x = base64.b16encode(x)
+x = base64.b16encode(x)
+x = base64.b16encode(x)
+'''
 
-print(s)
+print(x)
 
