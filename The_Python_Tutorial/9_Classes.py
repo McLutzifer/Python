cdb503231006ce1b85cfcb4828cd2cfb86a9d33a
McLutzifer <lukasschumi@gmx.at>
2021-08-04 17:59:19 +0200

    Random Remarks

--- The_Python_Tutorial/9_Classes.py
+++ The_Python_Tutorial/9_Classes.py
@@ -66,3 +66,16 @@ print(d.tricks)
 
 print(d.kind)
 print(e.name)
+
+
+# Random Remarks
+
+class Warehouse:
+    purpose = 'storage'
+    region = 'west'
+
+w1 = Warehouse()
+print(w1.purpose, w1.region)
+w2 = Warehouse()
+w2.region = 'east'
+print(w2.purpose, w2.region)
