023fc024fa7a0de22c5347605a3df83a29cc7742
McLutzifer <lukasschumi@gmx.at>
2021-08-05 16:30:11 +0200

    File wildcards

--- The_Python_Tutorial/10_Brief_tour_Standard_Lib.py
+++ The_Python_Tutorial/10_Brief_tour_Standard_Lib.py
@@ -1,6 +1,9 @@
 import os
 print(os.getcwd())
-os.system('mkdir today')
-os.system('touch test.txt')
-os.system('rm test.txt')
-os.system('rm -rf today')
+#os.system('mkdir today')
+#os.system('touch test.txt')
+#os.system('rm test.txt')
+#os.system('rm -rf today')
+
+import glob
+print(glob.glob('*.py'))
