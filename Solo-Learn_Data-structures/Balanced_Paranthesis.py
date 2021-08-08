c3b73046854b8e11aa9522986a6a07b37c184825
McLutzifer <lukasschumi@gmx.at>
2021-08-08 10:55:43 +0200

    create test cases

---
+++ Solo-Learn_Data-structures/Balanced_Paranthesis.py
@@ -0,0 +1,11 @@
+def balanced(expression):
+    pass
+
+
+test1 = '(ui)((uiuisdfds(df(dssds))))'
+test2 = '(sdfdsfsd(dsfsfsd((sdfdsf())))'
+test3 = ') dsfdsf () ('
+
+balanced(test1)
+balanced(test2)
+balanced(test3)
