c675792db45402fd51de6b4c37892cd495a695bf
McLutzifer <lukasschumi@gmx.at>
2021-08-08 18:35:44 +0200

    add doctest

--- The_Python_Tutorial/10_11_Quality_Control.py
+++ The_Python_Tutorial/10_11_Quality_Control.py
@@ -1,1 +1,13 @@
 import unittest
+import doctest
+
+
+def average(values):
+    """ Computes the arithmetic mean of a list of numbers.
+    
+    >>> print(average([20, 30, 70]))
+    40.0
+    """
+    return sum(values) / len(values)
+
+print(doctest.testmod())    # automatically validate the embedded tests
