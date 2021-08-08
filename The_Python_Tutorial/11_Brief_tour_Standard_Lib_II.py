ac9ec34433ba8c072683ad5c25206b14c9c98ebb
McLutzifer <lukasschumi@gmx.at>
2021-08-08 19:50:45 +0200

    Templating

--- The_Python_Tutorial/11_Brief_tour_Standard_Lib_II.py
+++ The_Python_Tutorial/11_Brief_tour_Standard_Lib_II.py
@@ -1,5 +1,5 @@
-import reprlib, pprint, textwrap
-
+import reprlib, pprint, textwrap, locale
+from string import Template
 
 print(reprlib.repr(set('supercalifragilisticexpialidocious')))
 
@@ -9,5 +9,12 @@ pprint.pprint(t, width = 30)
 doc = """The wrap() method is just like fill() except that it returns
  a list of strings instead of one big string with newlines to separate
  the wrapped lines."""
-
 print(textwrap.fill(doc, width = 40)) 
+
+#locale.setlocale(locale.LC_ALL, 'English_United States.1252')
+#conv = locale.localeconv()    # get a mapping of conventions
+
+# Templating
+
+t = Template('${village}folk send $$10 to $cause.')
+print(t.substitute(village='Nottingham', cause = 'the ditch fund'))
