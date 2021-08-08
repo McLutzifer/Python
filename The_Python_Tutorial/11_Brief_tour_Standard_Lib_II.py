6471b402c9008f0dc601b96c62f71615cf0b7b6d
McLutzifer <lukasschumi@gmx.at>
2021-08-08 18:45:08 +0200

    add textwrap

--- The_Python_Tutorial/11_Brief_tour_Standard_Lib_II.py
+++ The_Python_Tutorial/11_Brief_tour_Standard_Lib_II.py
@@ -1,9 +1,13 @@
-import reprlib
-import pprint
+import reprlib, pprint, textwrap
 
 
 print(reprlib.repr(set('supercalifragilisticexpialidocious')))
 
 t = [[[['black', 'cyan'], 'white', ['green', 'red']], [['magenta', 'yellow'], 'blue']]]
-
 pprint.pprint(t, width = 30)
+
+doc = """The wrap() method is just like fill() except that it returns
+ a list of strings instead of one big string with newlines to separate
+ the wrapped lines."""
+
+print(textwrap.fill(doc, width = 40)) 
