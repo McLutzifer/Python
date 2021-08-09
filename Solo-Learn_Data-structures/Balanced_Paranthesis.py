5fc2d32c9affe3b3197284b9a05fafb0a4be7e16
McLutzifer <50096558+McLutzifer@users.noreply.github.com>
2021-08-09 08:37:10 +0200

    eliminate sum

--- Solo-Learn_Data-structures/Balanced_Paranthesis.py
+++ Solo-Learn_Data-structures/Balanced_Paranthesis.py
@@ -24,14 +24,11 @@ False
 def balanced(expression):
     
     paranthesis = []
-    sum = 0
 
     for letter in expression:
         if letter == "(":
-            sum += 1
             paranthesis.append(letter)
         elif letter == ")":
-            sum -= 1
             if len(paranthesis) == 0:
                 return 'False'
             elif paranthesis[-1] == "(":
@@ -39,16 +36,10 @@ def balanced(expression):
             else:
                 return 'False'
     
-    if sum == 0:
+    if len(paranthesis) == 0:
         return 'True'
     else:
         return 'False'
 
+print(balanced(input()))
 
-test1 = '(ui)((uiuisdfds(df(dssds))))'
-test2 = '(sdfdsfsd(dsfsfsd((sdfdsf())))'
-test3 = ') dsfdsf () ('
-
-print(balanced(test1))
-print(balanced(test2))
-print(balanced(test3))
